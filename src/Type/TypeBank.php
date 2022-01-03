b06c588b121d42cead1bfe414fd33b1eabec50af
dionismda <dionismda@gmail.com>
2022-01-03 10:31:43 -0300

    add enumtrait in typebank

--- src/Type/TypeBank.php
+++ src/Type/TypeBank.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 namespace Futuralibs\Paymentslib\Type;
 
+use Futuralibs\Futurautils\Trait\EnumTrait;
+
 enum TypeBank: String {
+
+    use EnumTrait ;
 
     case BancoBrasil = '001';
 
