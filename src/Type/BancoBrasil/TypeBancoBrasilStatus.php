96e735eaa5a20450ca92ed26d1ac995bc8a67881
dionismda <dionismda@gmail.com>
2022-01-04 14:48:57 -0300

    alter return's type bancobrasilstatus

--- src/Type/BancoBrasil/TypeBancoBrasilStatus.php
+++ src/Type/BancoBrasil/TypeBancoBrasilStatus.php
@@ -3,25 +3,41 @@ declare(strict_types=1);
 
 namespace Futuralibs\Paymentslib\Type\BancoBrasil;
 
+use Exception;
 use Futuralibs\Futurautils\Trait\EnumTrait;
 
-enum TypeBancoBrasilStatus: String{
+enum TypeBancoBrasilStatus: int{
 
     use EnumTrait;
 
-    case ATIVA = 'ATIVA';
-    case CONCLUIDA = 'CONCLUIDA';
-    case REMOVIDA_PELO_USUARIO_RECEBEDOR = 'REMOVIDA_PELO_USUARIO_RECEBEDOR';
-    case REMOVIDA_PELO_PSP  = 'REMOVIDA_PELO_PSP ';
+    case ATIVA = 1;
+    case CONCLUIDA = 2;
+    case REMOVIDA_PELO_USUARIO_RECEBEDOR = 3;
+    case REMOVIDA_PELO_PSP  = 4;
 
-    public function getIndex(): int
+    public function getName(): string
     {
         return match($this)
         {
-            self::ATIVA => 1,
-            self::CONCLUIDA => 2,
-            self::REMOVIDA_PELO_USUARIO_RECEBEDOR => 3,
-            self::REMOVIDA_PELO_PSP => 4,
+            self::ATIVA => 'ATIVA',
+            self::CONCLUIDA => 'CONCLUIDA',
+            self::REMOVIDA_PELO_USUARIO_RECEBEDOR => 'REMOVIDA_PELO_USUARIO_RECEBEDOR',
+            self::REMOVIDA_PELO_PSP => 'REMOVIDA_PELO_PSP',
+        };
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function find(string $status): TypeBancoBrasilStatus
+    {
+        return match($status)
+        {
+            self::ATIVA->getName() => self::ATIVA,
+            self::CONCLUIDA->getName() => self::CONCLUIDA,
+            self::REMOVIDA_PELO_USUARIO_RECEBEDOR->getName() => self::REMOVIDA_PELO_USUARIO_RECEBEDOR,
+            self::REMOVIDA_PELO_PSP->getName() => self::REMOVIDA_PELO_PSP,
+            default => throw new Exception('Unexpected match value'),
         };
     }
 
